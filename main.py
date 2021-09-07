2154f08e1f5403e1070a43ba1bbeb401b5a57e7e
minbasubo <suwh2600@naver.com>
2021-09-07 20:30:15 +0900

    first

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import calendar
 from datetime import datetime
 
+
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -12,19 +13,37 @@ async def on_ready():
     print(f"ID : {bot.user.id}")
     await bot.change_presence(activity=discord.Game("명령어 가이드 = !도움"))
 
-
 @bot.command()
-async def 디데이(ctx, std, edd):
+async def 디데이(ctx, sty, stm, std, edy, edm, edd):
     try:
-        starty = int(std.split('-')[0])
-        startm = int(std.split('-')[1])
-        startd = int(std.split('-')[2])
-        sd = (starty + startm + startd)
-        endy = int(edd.split('-')[0])
-        endm = int(edd.split('-')[1])
-        endd = int(edd.split('-')[2])
-        ed = (endy + endm + endd)
-        result = (sd - ed)
+        mon_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        st_year = int(sty)
+        st_month = int(stm)
+        st_day = int(std)
+        ed_year = int(edy)
+        ed_month = int(edm)
+        ed_day = int(edd)
+        if st_year % 4 == 0 and (st_year % 100 != 0 or st_year % 400 == 0):  # 윤년
+            mon_day[1] = 29
+        if ed_year % 4 == 0 and (ed_year % 100 != 0 or ed_year % 400 == 0):  # 윤년
+            mon_day[1] = 29
+
+        def day(month, day):
+            monthday_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            total = 0
+            for i in range(month - 1):
+                total += monthday_list[i]
+            total += day
+            return total - 1
+
+        def solution(st_month, st_day, ed_month, ed_day):
+            start_total = day(st_month, st_day)
+            end_total = day(ed_month, ed_day)
+            return end_total - start_total
+
+        y = (ed_year * 365) - (st_year * 365)
+        result = solution(st_month, st_day, ed_month, ed_day) + y
+
         if result > 0:
             D_Day = discord.Embed(title=f"D-Day : {result}일", color=0xEF746F)
             await ctx.send(embed=D_Day)
@@ -34,7 +53,6 @@ async def 디데이(ctx, std, edd):
     except:
         Title = discord.Embed(title="잘못된 형식을 입력했습니다. 다시 입력해 주세요.", color=0xEF746F)
         await ctx.send(embed=Title)
-
 
 @bot.command(name="달력") #달력 기능
 async def 달력(ctx, year):
@@ -48,7 +66,6 @@ async def 달력(ctx, year):
     elif not year.isdigit():
         Title = discord.Embed(title="잘못된 년도입니다. 다시 입력해 주세요.", color=0xEF746F)
         await ctx.send(embed=Title)
-
 
 @bot.command(name="메모") #메모장 기능
 async def 메모(ctx, *args):
@@ -77,7 +94,7 @@ async def 날짜(ctx, year, month):
 @bot.command()
 async def 도움(ctx):
     Title = discord.Embed(title="봇 사용법", color=0xEF746F)
-    Des = discord.Embed(description="디데이 : ex) !디데이 2020-03-01 2020-04-21\n"
+    Des = discord.Embed(description="디데이 : ex) !디데이 2020 3 1 2020 4 21\n"
                                     "달력 : ex) !달력 2020\n"
                                     "메모 : ex) !메모 내용(띄어쓰기도 포함 가능)\n"
                                     "날짜 : ex) !날짜 2020 3", color=0xEF746F)
